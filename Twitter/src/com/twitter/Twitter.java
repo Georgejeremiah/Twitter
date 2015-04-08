8d42b987b95465e039bb78d535a3033a8883899b
Georgejeremiah <djolekg93@hotmail.com>
2015-04-08 15:04:02 +0200

    Napravljena javadoc dokumentacija za klasu Twitter njene atribute i metode(koje na 2 mesta imaju gresku)

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -3,16 +3,37 @@ package com.twitter;
 import java.util.LinkedList;
 
 import com.twitter.poruka.TwitterPoruka;
-
+/**
+ * 
+ * @author Georgejeremiah
+ *
+ */
     public class Twitter {
+    	/**
+    	 *  privatni atribut lista twitter poruka
+    	 */
 	                private LinkedList<TwitterPoruka> poruke=new LinkedList<TwitterPoruka>();
+	                /**
+	                 * metoda koja prima 2 parametra 
+	                 * @param korisnik 
+	                 * @param poruka
+	                 * kreira novi objekat tipa TwitterPoruka, 
+	                 * dodeljuje mu vrednosti unetih parametara i 
+	                 * ubacuje taj objekat na poslednje mesto u listi
+	                 */
     public void unesi(String korisnik,String poruka){
 	            TwitterPoruka tp=new TwitterPoruka();
 	            tp.setKorisnik(korisnik);
 	            tp.setPoruka(poruka);
-	            poruke.add(tp);
+	            poruke.addLast(tp);
 	           }
-     
+     /**
+      * metoda kao ulaz prima 2 parametra:
+      * @param maxBroj broj poruka sa unetim tagom koje zelimo da izlistamo
+      * (istovremeno i kapacitet niza koji metoda vraca kao povratnu vrednost)
+      * @param tag parametar na osnovu koga pretrazujemo listu poruka
+      * @return metoda vraca niz poruka koje sadrze uneti tag
+      */
     public TwitterPoruka[] vratiPoruke(int maxBroj, String tag){
     	if(tag==null || tag.isEmpty()){
     		throw new RuntimeException("Morate uneti tag");}
@@ -23,7 +44,7 @@ import com.twitter.poruka.TwitterPoruka;
     	for(int i=0;i<poruke.size();i++){
     		if(poruke.get(i).getPoruka().indexOf(tag)!=-1){
     			if(brojac<maxBroj){
-    				rezultat[brojac]=poruke.get(i);
+    				rezultat[brojac+1]=poruke.get(i);
     				brojac++;
     				}
     			}
